2efb30d6d4bbf16620a617a5c244492001d69a75
Gavyn McKenzie <gavyn@etchapps.com>
2016-03-07 14:15:29 +0000

    Add IE filter

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function ie9to11() {
+  const ua = window.navigator.userAgent;
+  if (ua.indexOf('Trident/7.0') > 0) {
+    return true;
+  } else if (ua.indexOf('Trident/6.0') > 0) {
+    return true;
+  } else if (ua.indexOf('Trident/5.0') > 0) {
+    return true;
+  }
+  return false;  // not IE9, 10 or 11
+}
+
 function fixThisSVG(svg) {
   const width = svg.getAttribute('width');
   const height = svg.getAttribute('height');
 
-  if (width && height && !svg.getAttribute('data-ignore-svg-polyfill')) {
+  if (width && height && !svg.getAttribute('data-ignore-svg-polyfill') && ie9to11()) {
     const ratio = (parseInt(height, 10) / parseInt(width, 10) * 100) + '%';
     const wrapper = document.createElement('div');
     const spacer = document.createElement('div');
