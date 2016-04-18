941afcb57f7ade28db01a41a983792f69acffc59
Gavyn McKenzie <gavyn.mckenzie@gmail.com>
2016-04-18 17:18:19 +0100

    Update index.js
    Add android native to the list of crap browsers

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function ie9to11() {
+function isCrapBrowser() {
+  //return true;
   const ua = window.navigator.userAgent;
   if (ua.indexOf('Trident/7.0') > 0) {
     return true;
   } else if (ua.indexOf('Trident/6.0') > 0) {
     return true;
   } else if (ua.indexOf('Trident/5.0') > 0) {
+    return true;
+  } else if ((nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 &&     nua.indexOf('AppleWebKit') > -1) && !(nua.indexOf('Chrome') > -1)) {
     return true;
   }
   return false;  // not IE9, 10 or 11
@@ -14,7 +17,7 @@ function fixThisSVG(svg) {
   const width = svg.getAttribute('width');
   const height = svg.getAttribute('height');
 
-  if (width && height && !svg.getAttribute('data-ignore-svg-polyfill') && ie9to11()) {
+  if (width && height && !svg.getAttribute('data-ignore-svg-polyfill') && isCrapBrowser()) {
     const ratio = (parseInt(height, 10) / parseInt(width, 10) * 100) + '%';
     const wrapper = document.createElement('div');
     const spacer = document.createElement('div');
@@ -26,7 +29,7 @@ function fixThisSVG(svg) {
 
     svg.parentNode.appendChild(wrapper);
 
-    svg.setAttribute('style', 'display: block; width: 100%; position: absolute; top: 0; left: 0;');
+    svg.setAttribute('style', 'display: block; width: 100%; position: absolute; top: 0; left: 0; bottom: 0;');
 
     wrapper.appendChild(svg);
   }
